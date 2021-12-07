0e8aee38095f461db01e809fe1a3ed82a6ee9ac3
Tony Kang <aeokiss@gmail.com>
2021-12-07 13:33:01 +0900

    init

---
+++ vs2019_clean.bat
@@ -0,0 +1,11 @@
+@echo off
+
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+cd zlib
+
+nmake -f ..\res\Makefile.msvc clean
+
+cd ..
+
+pause
---
+++ vs2022_build_Win32.bat
@@ -0,0 +1,41 @@
+@echo off
+
+@call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+cd zlib
+
+nmake -f ..\res\Makefile.msvc clean
+nmake -f ..\res\Makefile.msvc NO_NLS=1 MFLAGS=-MTd DEBUG=1
+
+cd ..
+
+@set OutputLibDir=build\vs2022\Win32\Debug
+
+rmdir /S /Q %OutputLibDir%
+mkdir %OutputLibDir%
+
+copy zlib\zlib.lib %OutputLibDir%
+copy zlib\zlib.h %OutputLibDir%
+copy zlib\zconf.h %OutputLibDir%
+copy zlib\*.exe %OutputLibDir%
+copy zlib\*.pdb %OutputLibDir%
+
+cd zlib
+
+nmake -f ..\res\Makefile.msvc clean
+nmake -f ..\res\Makefile.msvc NO_NLS=1 MFLAGS=-MT
+
+cd ..
+
+@set OutputLibDir=build\vs2022\Win32\Release
+
+rmdir /S /Q %OutputLibDir%
+mkdir %OutputLibDir%
+
+copy zlib\zlib.lib %OutputLibDir%
+copy zlib\zlib.h %OutputLibDir%
+copy zlib\zconf.h %OutputLibDir%
+copy zlib\*.exe %OutputLibDir%
+copy zlib\*.pdb %OutputLibDir%
+
+pause
